d663a5bc3c87cdb43cabb162dddb768e9d705382
ztd <159930834+Vande541@users.noreply.github.com>
2026-05-05 16:26:41 +0700

    Add files via upload

--- UnityPy_AOV/__init__.py
+++ UnityPy_AOV/__init__.py
@@ -1,1 +1,12 @@
-from . import MeshRendererExporter, SpriteHelper, Texture2DConverter, AudioClipConverter, MeshExporter
+__version__ = "1.9.24"
+
+from .environment import Environment
+#from .helpers.ArchiveStorageManager import set_assetbundle_decrypt_key
+
+
+def load(*args):
+    return Environment(*args)
+
+
+# backward compatibility
+AssetsManager = Environment
